fcd88b680232e8ef442db93fd53fe5ea5c259025
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2025-05-08 01:36:31 -0500

    refactoring outside of my-site

---
+++ src/marketing-page/components/FAQ.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function FAQ() {
+  const [expanded, setExpanded] = React.useState<string[]>([]);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(
+        isExpanded
+          ? [...expanded, panel]
+          : expanded.filter((item) => item !== panel),
+      );
+    };
+
+  return (
+    <Container
+      id="faq"
+      sx={{
+        pt: { xs: 4, sm: 12 },
+        pb: { xs: 8, sm: 16 },
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: { xs: 3, sm: 6 },
+      }}
+    >
+      <Typography
+        component="h2"
+        variant="h4"
+        sx={{
+          color: 'text.primary',
+          width: { sm: '100%', md: '60%' },
+          textAlign: { sm: 'left', md: 'center' },
+        }}
+      >
+        FAQ
+      </Typography>
+      <Box sx={{ width: '100%' }}>
+        <Accordion
+          expanded={expanded.includes('panel1')}
+          onChange={handleChange('panel1')}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1d-content"
+            id="panel1d-header"
+          >
+            <Typography component="span" variant="subtitle2">
+              Who are the developers?
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography
+              variant="body2"
+              gutterBottom
+              sx={{ maxWidth: { sm: '100%', md: '70%' } }}
+            >
+              Northwestern CS Grads 
+              <br /><br />
+              <Link
+              href="https://www.linkedin.com/in/isaacsun1/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Isaac Sun
+              </Link>
+              <br />
+              <Link
+              href="https://www.linkedin.com/in/mkli/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Mark Li
+              </Link>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        
+      </Box>
+    </Container>
+  );
+}
