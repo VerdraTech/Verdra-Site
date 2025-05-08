dbfee04fec195e333cc6b270ebaef99a90b02fe4
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2025-05-08 01:10:29 -0500

    set up for deployment

--- my-site/vite.config.ts
+++ my-site/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js']
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
   define: {
-    'process.env': {}
-  }
+    "process.env": {},
+  },
+  base: "/MVP/",
+  build: {
+    outDir: "build",
+  },
 });
